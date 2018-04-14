87cfc98a8393d702aca18a09c592974ed120945f
StardustGogeta <liam.gerst@gmail.com>
2018-04-14 11:25:44 -0400

    Preliminary account data

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 
 var express = require("express");
 var path = require("path");
+var jsonfile = require("jsonfile");
 
 app = express();
 app.use("/", express.static(__dirname));
 app.use(express.urlencoded());
 app.listen(process.env.PORT || 3000, function() { console.log('Node.JS is listening! Port '+(process.env.PORT || 3000)); });
+
+data = jsonfile.readFileSync("data.json");
+console.log(data);
